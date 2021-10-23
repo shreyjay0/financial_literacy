03e36946488f3ef2b434c3ecf0935435ec5f5ffd
Jim Su <jimsu2012@gmail.com>
2021-10-22 22:31:57 -0400

    Add view stubs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import ExplorePage from "./pages/ExplorePage";
+import HabitsPage from "./pages/HabitsPage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="" component={IndexPage} />
+      <Route path="explore" component={ExplorePage} />
+      <Route path="habits" component={HabitsPage} />
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/HabitsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HabitsPage = () => {
+  return <div></div>;
+};
+
+export default HabitsPage;
