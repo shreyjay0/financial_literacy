140e2d3731debda737611687e398ae30069f6d62
ze2 <62995130+ze-2@users.noreply.github.com>
2021-10-23 11:52:58 +0800

    starter

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "../style/IndexPage.css";
 
+
 const IndexPage = () => {
-  return <div></div>;
+  return (
+    <div className="index">
+      <div className="header">
+        <h3>Learn how you can be</h3>
+        <h1>Financially Independent</h1>
+      </div>
+      <div className="search-box">
+        <input
+            type="text"
+            id="searchBar"
+            placeholder="Search"
+        />
+        <button type="submit">Search</button>
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
