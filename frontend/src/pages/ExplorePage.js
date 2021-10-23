9725516a69d2e5fdddf97f9cd7f2795fec0af499
Jim Su <jimsu2012@gmail.com>
2021-10-22 22:36:08 -0400

    Add CSS files for page style

--- frontend/src/pages/ExplorePage.js
+++ frontend/src/pages/ExplorePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../style/ExplorePage.css";
 
 const ExplorePage = () => {
   return <div></div>;
--- frontend/src/pages/HabitsPage.js
+++ frontend/src/pages/HabitsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../style/HabitsPage.css";
 
 const HabitsPage = () => {
   return <div></div>;
